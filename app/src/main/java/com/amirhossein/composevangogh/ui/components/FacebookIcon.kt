dc5b43e464984d8ab0089d9126c2e03bd822d9db
amirhossein <amirhossein.jahanshahi13@gmail.com>
2023-04-19 17:29:15 +0330

    add facebook icon

---
+++ app/src/main/java/com/amirhossein/composevangogh/ui/components/FacebookIcon.kt
@@ -0,0 +1,39 @@
+package com.amirhossein.composevangogh.ui.components
+
+import android.graphics.Paint
+import android.graphics.Typeface
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FacebookIcon(modifier: Modifier = Modifier) {
+    val assetManager = LocalContext.current.assets
+    val paint = Paint().apply {
+        textAlign = Paint.Align.CENTER
+        textSize = 200f
+        color = Color.White.toArgb()
+        typeface = Typeface.createFromAsset(assetManager,"fonts/FACEBOLF.OTF")
+    }
+    Canvas(
+        modifier =
+        Modifier
+            .size(100.dp)
+            .padding(16.dp)
+            .then(modifier)
+    ) {
+        drawRoundRect(
+            color= Color(0xFF1776d1),
+            cornerRadius = CornerRadius(20f,20f),
+        )
+        drawContext.canvas.nativeCanvas.drawText("f",center.x + 25 , center.y + 90 , paint)
+    }
+}
--- app/src/main/java/com/amirhossein/composevangogh/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/amirhossein/composevangogh/ui/screen/HomeScreen.kt
@@ -1,15 +1,19 @@
 package com.amirhossein.composevangogh.ui.screen
 
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.amirhossein.composevangogh.ui.components.FacebookIcon
 import com.amirhossein.composevangogh.ui.components.InstagramIcons
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Row(modifier = Modifier.fillMaxSize()) {
         InstagramIcons()
+        Spacer(modifier = Modifier.width(10.dp))
+        FacebookIcon()
+        Spacer(modifier = Modifier.width(10.dp))
     }
 }
